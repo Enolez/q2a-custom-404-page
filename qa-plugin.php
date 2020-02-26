1ee07d53180a73518f2e2b8b6530c37505f81e49
Enolez <60704849+Enolez@users.noreply.github.com>
2020-02-26 17:44:16 +0600

    Update File Name

--- qa-plugin.php
+++ qa-plugin.php
@@ -1,16 +1,16 @@
 <?php
 
 /*
-	Plugin Name: Custom 404 Page
-	Plugin URI: https://github.com/amiyasahu/q2a-custom-404-page
-	Plugin Description: Customized 404 page for Question2Answer site 
+	Plugin Name:Enolez Error Page
+	Plugin URI: https://github.com/Enolez/q2a-custom-404-page
+	Plugin Description: Special Error page for Enolez 
 	Plugin Version: 1.1
 	Plugin Date: 2014-07-25
-	Plugin Author: Amiya Sahu 
-	Plugin Author URI: http://amiyasahu.com/
+	Plugin Author: Masud 
+	Plugin Author URI: https://enolez.com/আব্দুল্লাহ+আল+মাসুদ
 	Plugin License: GPLv2
 	Plugin Minimum Question2Answer Version: 1.3.2
-	Plugin Update Check URI: https://raw.githubusercontent.com/amiyasahu/q2a-custom-404-page/master/qa-plugin.php
+	Plugin Update Check URI: https://enolez.com/Enolez/q2a-custom-404-page
 */
 /*
 	NOTE : This plugin is forked from https://github.com/ProThoughts/q2a-custom-404-page 
